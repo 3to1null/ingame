a06ea1291d13973a9a6606e6ce19204533beaee8
timkleintk <timvdlr@gmail.com>
2020-03-09 18:15:55 +0000

    setting up the game

--- frontend/sketch.js
+++ frontend/sketch.js
@@ -1,5 +1,27 @@
+let bullet;
 
 function setup() {
-    createCanvas(600,800);
+    createCanvas(windowWidth, windowHeight);
+    bullet = new Bullet(100,100,0,0);
 }
 
+function draw() {
+    background(51);
+    bullet.x = mouseX;
+    bullet.y = mouseY;
+    bullet.draw();
+}
+
+
+
+
+
+function Bullet(x,y,h,v) { //the class for a bullet
+    this.pos = createVector(x,y);
+    this.vel = createVector(h,v);
+
+    this.draw = function () {
+        rect(this.x, this.y, 20, 20);
+    }
+
+}
