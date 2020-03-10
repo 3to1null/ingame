83fbd268c32d2d8ea1f31d83282539e8474bb290
Nathan Klumpenaar <nathanklumpenaar10@outlook.com>
2020-03-10 14:33:05 +0000

    am id doing this gud?

--- frontend/sketch.js
+++ frontend/sketch.js
@@ -2,16 +2,17 @@ let bullet;
 
 function setup() {
     createCanvas(400, 400);
-    bullet = new Bullet(100,100,0,0);
+    bullet = new Bullet(100,100,0,1);
 }
 
 function draw() {
-    background(51);
-    bullet.x = mouseX;
-    bullet.y = mouseY;
+    background(51,52,0);
+    //bullet.x = mouseX;
+    //bullet.y = mouseY;
+    bullet.update();
     bullet.draw();
-    stroke(255);
-    rect(10,10,10,10);
+    
+    //rect(10,10,10,10);
 }
 
 
@@ -20,11 +21,16 @@ function draw() {
 
 
 class Bullet {
-    constructor(x, y, h, v) {
+    constructor(x, y, r, v) {
         this.pos = createVector(x, y);
-        this.vel = createVector(h, v);
+        this.v = v;
+        this.r = r;
         this.draw = function () {
             rect(this.x, this.y, 20, 20);
         };
+        this.update = function() {
+            this.pos.x += cos(r)*v;
+            this.pos.y += sin(r)*v;
+        }
     }
 }
