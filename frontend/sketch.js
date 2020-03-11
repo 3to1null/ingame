04b3620707808d1b9c34764670a9bdf20a7bd398
timkleintk <timvdlr@gmail.com>
2020-03-11 09:54:54 +0100

    TWO TANKS (and server updates)

--- frontend/sketch.js
+++ frontend/sketch.js
@@ -1,21 +1,46 @@
 let maxV = 3;
 
 let player; 
+let enemy;
 
 let inputs = [false,false,false,false,false]; // left, right, up, down, fire
 let bullets = [];
 
+// ------- server stuff
+var socket = io('timklein.tk:8009');
+
+socket.on('init', (data) => {
+    console.log(data);
+});
+
+socket.on('update_state', (data) => {
+    console.log('update_state')
+    loop();
+})
+
+
+
+
+// ------- main game code
+
 function setup() {
     createCanvas(400, 400);
     rectMode(CENTER);
-    player = new Tank(100,100,0,0);
+    player = new Player(0, color(0,255,0),100,100,0,0);
+    enemy = new Enemy(1, color(255,0,0), 300, 300, 0, 0);
+    noLoop();
 }
 
 function draw() {
     background(51);
+    stroke(0);
+    text("fps: " + round(frameRate()), 5, 10);
+    
     player.update();
     player.draw();
-    
+    enemy.update();
+    enemy.draw();
+    noLoop();
 }
 
 function keyPressed() {
@@ -83,18 +108,23 @@ class Bullet {
 }
 
 
-class Tank {
-    constructor(x, y, r, v) {
+class Player {
+    constructor(id, c, x, y, r, v) {
+        this.id = id;
         this.x = x;
         this.y = y;
         this.r = r;
         this.v = v;
+        this.c = c;
 
         this.draw = function() {
             translate(this.x, this.y);
             rotate(this.r);
+            stroke(this.c);
             rect(0, 0, 20, 15);
             rect(10,0,15,3);
+            rotate(-this.r);
+            translate(-this.x, -this.y);
         };
 
         this.update = function() {
@@ -106,6 +136,50 @@ class Tank {
                 this.v += 0.1;
             if (inputs[3])
                 this.v -= 0.1;
+
+            this.v = cap(this.v, 0, maxV);
+            
+            this.x += cos(this.r)*this.v;
+            this.y += sin(this.r)*this.v;
+            
+            this.x = cap(this.x, 0, width);
+            this.y = cap(this.y, 0, height);
+        };
+
+        this.rotate = function(dr) {
+            this.r += dr;
+        };
+    }
+}
+
+class Enemy {
+    constructor(id, c, x, y, r, v) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.v = v;
+        this.c = c;
+
+        this.draw = function() {
+            translate(this.x, this.y);
+            rotate(this.r);
+            stroke(this.c);
+            rect(0, 0, 20, 15);
+            rect(10,0,15,3);
+            rotate(-this.r);
+            translate(-this.x, -this.y);
+        };
+
+        this.update = function() {
+            /*if (inputs[0])
+                this.rotate(-0.05);
+            if (inputs[1])
+                this.rotate(0.05);
+            if (inputs[2])
+                this.v += 0.1;
+            if (inputs[3])
+                this.v -= 0.1;*/
 
             this.v = cap(this.v, 0, maxV);
             
